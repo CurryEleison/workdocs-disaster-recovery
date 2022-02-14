123739668fc5695fb6d9430aef9f88cc77d9e720
Thomas Petersen <petersen@temp.dk>
2022-02-14 07:06:30 +0100

    Reformatted and extra logging

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from argparse import ArgumentParser
 import logging
 
-from workdocs_dr.cli_arguments import clients_from_input, bucket_url_from_input, logging_setup, organization_id_from_input, run_style_from_input, wdfilter_from_input
+from workdocs_dr.cli_arguments import (
+    clients_from_input,
+    bucket_url_from_input,
+    logging_setup,
+    organization_id_from_input,
+    run_style_from_input,
+    wdfilter_from_input,
+)
 from workdocs_dr.directory_backup import DirectoryBackupRunner
 
 rootlogger = logging.getLogger()
@@ -13,27 +20,38 @@ def main():
     # parser.add_argument("--action", default="backup", help="Action to take", choices=["backup"])
     parser.add_argument("--profile", help="AWS profile", default=None)
     parser.add_argument("--region", help="AWS region", default=None)
-    parser.add_argument("--organization-id",
-                        help="Workdocs organization id (directory id)", default=None)
+    parser.add_argument(
+        "--organization-id",
+        help="Workdocs organization id (directory id)",
+        default=None,
+    )
     parser.add_argument("--bucket-name", help="Name of bucket", default=None)
-    parser.add_argument(
-        "--prefix", help="Prefix for bucket access", default=None)
+    parser.add_argument("--prefix", help="Prefix for bucket access", default=None)
     parser.add_argument("--user-query", help="Query of user", default=None)
+    parser.add_argument("--folder", help="Folder(s) to backup up", default=None)
     parser.add_argument(
-        "--folder", help="Folder(s) to backup up", default=None)
-    parser.add_argument("--workdocs-role-arn",
-                        help="ARN of role that can access workdocs", default=None)
-    parser.add_argument("--run-style",
-                        help="Run a FULL or ACTIVITIES (incremental) backup. Default is autodetect",
-                        default=None)
+        "--workdocs-role-arn", help="ARN of role that can access workdocs", default=None
+    )
+    parser.add_argument(
+        "--run-style",
+        help="Run a FULL or ACTIVITIES (incremental) backup. Default is autodetect",
+        default=None,
+    )
     parser.add_argument(
         "--bucket-role-arn",
-        help="ARN of role that puts/gets disaster recovery documents", default=None)
-    parser.add_argument("--verbose", help="Verbose output",
-                        dest="verbose", action="store_true")
+        help="ARN of role that puts/gets disaster recovery documents",
+        default=None,
+    )
+    parser.add_argument(
+        "--verbose", help="Verbose output", dest="verbose", action="store_true"
+    )
     args = parser.parse_args()
-    clients = clients_from_input(profile_name=args.profile, region_name=args.region,
-                                 workdocs_role_arn=args.workdocs_role_arn, bucket_role_arn=args.bucket_role_arn)
+    clients = clients_from_input(
+        profile_name=args.profile,
+        region_name=args.region,
+        workdocs_role_arn=args.workdocs_role_arn,
+        bucket_role_arn=args.bucket_role_arn,
+    )
     organization_id = organization_id_from_input(args.organization_id)
     run_style = run_style_from_input(args.run_style)
     db = DirectoryBackupRunner(
@@ -46,8 +64,9 @@ def main():
     logging_setup(rootlogger=rootlogger, verbose=args.verbose)
     logging.info(f"orgid {db.organization_id} url {db.bucket_url}")
     db.runall()
+    logging.info("Finished backup run and exited normally")
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- workdocs_dr/directory_backup.py
+++ workdocs_dr/directory_backup.py
@@ -1,6 +1,6 @@
-
 from datetime import datetime, timedelta, timezone
 from enum import Enum, auto
+import logging
 from urllib.parse import urlparse
 from yaml import dump
 from workdocs_dr.activity_backup import ActivityBackupRunner
@@ -12,8 +12,15 @@ from workdocs_dr.user import UserHelper, UserKeyHelper
 from workdocs_dr.user_backup import UserBackupRunner
 
 
-class DirectoryBackupRunner():
-    def __init__(self, clients: AwsClients, organization_id: str, bucket_url: str, filter: WdFilter = None, run_style: RunStyle = None) -> None:
+class DirectoryBackupRunner:
+    def __init__(
+        self,
+        clients: AwsClients,
+        organization_id: str,
+        bucket_url: str,
+        filter: WdFilter = None,
+        run_style: RunStyle = None,
+    ) -> None:
         self.clients = clients
         self.organization_id = organization_id
         self.bucket_url = bucket_url
@@ -23,19 +30,28 @@ class DirectoryBackupRunner():
 
     def get_minder(self):
         if self.minder is None:
-            self.minder = DirectoryBackupMinder(self.clients, self.organization_id, self.bucket_url)
+            self.minder = DirectoryBackupMinder(
+                self.clients, self.organization_id, self.bucket_url
+            )
         return self.minder
 
     def runall(self):
         directory = WdDirectory(self.organization_id, self.clients)
 
         run_style = self.forced_runstyle or self.get_minder().get_best_run_style()
+        logging.info(f"Starting Backup. Runstyle is {run_style}")
         if run_style is RunStyle.ABORT:
             return
         if run_style is RunStyle.ACTIVITIES:
             self._update_event_time(RunStyle.ACTIVITIES, RunEvent.START)
             # TODO: Implement user filters -- not at all trivial due to sharing, but we'll do it some day
-            abr = ActivityBackupRunner(self.clients, self.organization_id, self.bucket_url, directory, self.minder)
+            abr = ActivityBackupRunner(
+                self.clients,
+                self.organization_id,
+                self.bucket_url,
+                directory,
+                self.minder,
+            )
             results = abr.backup_activity_queue()
             self._update_event_time(RunStyle.ACTIVITIES, RunEvent.END)
             return results
@@ -51,6 +67,11 @@ class DirectoryBackupRunner():
         return results
 
     def _update_event_time(self, run_style: RunStyle, run_event: RunEvent) -> None:
-        if self.filter is None or ((self.filter.foldernames is None or len(self.filter.foldernames) == 0)
-                                   and self.filter.userquery is None and self.filter.folderpattern is None):
-            self.get_minder().update_last_event_time(run_style=run_style, run_event=run_event)
+        if self.filter is None or (
+            (self.filter.foldernames is None or len(self.filter.foldernames) == 0)
+            and self.filter.userquery is None
+            and self.filter.folderpattern is None
+        ):
+            self.get_minder().update_last_event_time(
+                run_style=run_style, run_event=run_event
+            )
